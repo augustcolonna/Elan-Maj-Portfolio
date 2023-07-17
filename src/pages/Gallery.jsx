a6376e022063c5def9fb30d0aace66e29937f2df
Gus <august.colonna@gmail.com>
2023-07-17 20:14:46 +0200

    images on homepage working properly now, need to create a function on the Gallery page, using a for loop to set the url to gallery images. Deployed version is working - had to change the vite.config file, as welll as the dist folder as base folder. Main problem was getting images in the asset folder to move to the deployed folder.

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -5,167 +5,171 @@ import '../styles/Gallery.scss'
 function Gallery() {
   const images = [
     {
-      original: 'src/assets/_MG_0415.jpg',
-      thumbnail: 'src/assets/_MG_0415.jpg',
+      original: '_MG_0415.jpg',
+      thumbnail: '_MG_0415.jpg',
     },
     {
-      original: 'src/assets/_MG_0427.jpg',
-      thumbnail: 'src/assets/_MG_0427.jpg',
+      original: '_MG_0427.jpg',
+      thumbnail: '_MG_0427.jpg',
     },
     {
-      original: 'src/assets/_MG_0460.jpg',
-      thumbnail: 'src/assets/_MG_0460.jpg',
+      original: '_MG_0460.jpg',
+      thumbnail: '_MG_0460.jpg',
     },
     {
-      original: 'src/assets/_MG_0473.jpg',
-      thumbnail: 'src/assets/_MG_0473.jpg',
+      original: '_MG_0473.jpg',
+      thumbnail: '_MG_0473.jpg',
     },
     {
-      original: 'src/assets/CPIMG_5431.jpg',
-      thumbnail: 'src/assets/CPIMG_5431.jpg',
+      original: 'CPIMG_5431.jpg',
+      thumbnail: 'CPIMG_5431.jpg',
     },
     {
-      original: 'src/assets/CPIMG_5463.jpg',
-      thumbnail: 'src/assets/CPIMG_5463.jpg',
+      original: 'CPIMG_5463.jpg',
+      thumbnail: 'CPIMG_5463.jpg',
     },
     {
-      original: 'src/assets/CPIMG_5488.jpg',
-      thumbnail: 'src/assets/CPIMG_5488.jpg',
+      original: 'CPIMG_5488.jpg',
+      thumbnail: 'CPIMG_5488.jpg',
     },
     {
-      original: 'src/assets/CPIMG_5496.jpg',
-      thumbnail: 'src/assets/CPIMG_5496.jpg',
+      original: 'CPIMG_5496.jpg',
+      thumbnail: 'CPIMG_5496.jpg',
     },
     {
-      original: 'src/assets/CPIMG_5564.jpg',
-      thumbnail: 'src/assets/CPIMG_5564.jpg',
+      original: 'CPIMG_5564.jpg',
+      thumbnail: 'CPIMG_5564.jpg',
     },
     {
-      original: 'src/assets/CPIMG_5581.jpg',
-      thumbnail: 'src/assets/CPIMG_5581.jpg',
+      original: 'CPIMG_5581.jpg',
+      thumbnail: 'CPIMG_5581.jpg',
     },
     {
-      original: 'src/assets/EL236A1709.jpg',
-      thumbnail: 'src/assets/EL236A1709.jpg',
+      original: 'EL236A1709.jpg',
+      thumbnail: 'EL236A1709.jpg',
     },
     {
-      original: 'src/assets/EL236A1740.jpg',
-      thumbnail: 'src/assets/EL236A1740.jpg',
+      original: 'EL236A1740.jpg',
+      thumbnail: 'EL236A1740.jpg',
     },
     {
-      original: 'src/assets/EL236A1769.jpg',
-      thumbnail: 'src/assets/EL236A1769.jpg',
+      original: 'EL236A1769.jpg',
+      thumbnail: 'EL236A1769.jpg',
     },
     {
-      original: 'src/assets/EL236A1788.jpg',
-      thumbnail: 'src/assets/EL236A1788.jpg',
+      original: 'EL236A1788.jpg',
+      thumbnail: 'EL236A1788.jpg',
     },
     {
-      original: 'src/assets/EL236A1793.jpg',
-      thumbnail: 'src/assets/EL236A1793.jpg',
+      original: 'EL236A1793.jpg',
+      thumbnail: 'EL236A1793.jpg',
     },
     {
-      original: 'src/assets/EL236A1851.jpg',
-      thumbnail: 'src/assets/EL236A1851.jpg',
+      original: 'EL236A1851.jpg',
+      thumbnail: 'EL236A1851.jpg',
     },
     {
-      original: 'src/assets/EL236A1906.jpg',
-      thumbnail: 'src/assets/EL236A1906.jpg',
+      original: 'EL236A1906.jpg',
+      thumbnail: 'EL236A1906.jpg',
     },
     {
-      original: 'src/assets/FAIMG_6733.jpg',
-      thumbnail: 'src/assets/FAIMG_6733.jpg',
+      original: 'FAIMG_6733.jpg',
+      thumbnail: 'FAIMG_6733.jpg',
     },
     {
-      original: 'src/assets/FAIMG_6837.jpg',
-      thumbnail: 'src/assets/FAIMG_6837.jpg',
+      original: 'FAIMG_6837.jpg',
+      thumbnail: 'FAIMG_6837.jpg',
     },
     {
-      original: 'src/assets/FAIMG_6846.jpg',
-      thumbnail: 'src/assets/FAIMG_6846.jpg',
+      original: 'FAIMG_6846.jpg',
+      thumbnail: 'FAIMG_6846.jpg',
     },
     {
-      original: 'src/assets/G-IMG_2464.jpg',
-      thumbnail: 'src/assets/G-IMG_2464.jpg',
+      original: 'G-IMG_2464.jpg',
+      thumbnail: 'G-IMG_2464.jpg',
     },
     {
-      original: 'src/assets/G-IMG_2528.jpg',
-      thumbnail: 'src/assets/G-IMG_2528.jpg',
+      original: 'G-IMG_2528.jpg',
+      thumbnail: 'G-IMG_2528.jpg',
     },
     {
-      original: 'src/assets/G-IMG_2685.jpg',
-      thumbnail: 'src/assets/G-IMG_2685.jpg',
+      original: 'G-IMG_2685.jpg',
+      thumbnail: 'G-IMG_2685.jpg',
     },
     {
-      original: 'src/assets/G-IMG_2690.jpg',
-      thumbnail: 'src/assets/G-IMG_2690.jpg',
+      original: 'G-IMG_2690.jpg',
+      thumbnail: 'G-IMG_2690.jpg',
     },
     {
-      original: 'src/assets/G-IMG_2711.jpg',
-      thumbnail: 'src/assets/G-IMG_2711.jpg',
+      original: 'G-IMG_2711.jpg',
+      thumbnail: 'G-IMG_2711.jpg',
     },
     {
-      original: 'src/assets/G-IMG_2718.jpg',
-      thumbnail: 'src/assets/G-IMG_2718.jpg',
+      original: 'G-IMG_2718.jpg',
+      thumbnail: 'G-IMG_2718.jpg',
     },
     {
-      original: 'src/assets/G-IMG_2859.jpg',
-      thumbnail: 'src/assets/G-IMG_2859.jpg',
+      original: 'G-IMG_2859.jpg',
+      thumbnail: 'G-IMG_2859.jpg',
     },
     {
-      original: 'src/assets/G-IMG_4574.jpg',
-      thumbnail: 'src/assets/G-IMG_4574.jpg',
+      original: 'G-IMG_4574.jpg',
+      thumbnail: 'G-IMG_4574.jpg',
     },
     {
-      original: 'src/assets/ISIMG_6052.jpg',
-      thumbnail: 'src/assets/ISIMG_6052.jpg',
+      original: 'ISIMG_6052.jpg',
+      thumbnail: 'ISIMG_6052.jpg',
     },
     {
-      original: 'src/assets/ISIMG_6077.jpg',
-      thumbnail: 'src/assets/ISIMG_6077.jpg',
+      original: 'ISIMG_6077.jpg',
+      thumbnail: 'ISIMG_6077.jpg',
     },
     {
-      original: 'src/assets/SFIMG_6600.jpg',
-      thumbnail: 'src/assets/SFIMG_6600.jpg',
+      original: 'SFIMG_6600.jpg',
+      thumbnail: 'SFIMG_6600.jpg',
     },
     {
-      original: 'src/assets/SFIMG_6617.jpg',
-      thumbnail: 'src/assets/SFIMG_6617.jpg',
+      original: 'SFIMG_6617.jpg',
+      thumbnail: 'SFIMG_6617.jpg',
     },
     {
-      original: 'src/assets/SFIMG_6619.jpg',
-      thumbnail: 'src/assets/SFIMG_6619.jpg',
+      original: 'SFIMG_6619.jpg',
+      thumbnail: 'SFIMG_6619.jpg',
     },
     {
-      original: 'src/assets/SLPIMG_5708.jpg',
-      thumbnail: 'src/assets/SLPIMG_5708.jpg',
+      original: 'SLPIMG_5708.jpg',
+      thumbnail: 'SLPIMG_5708.jpg',
     },
     {
-      original: 'src/assets/SLPIMG_5711.jpg',
-      thumbnail: 'src/assets/SLPIMG_5711.jpg',
+      original: 'SLPIMG_5711.jpg',
+      thumbnail: 'SLPIMG_5711.jpg',
     },
     {
-      original: 'src/assets/SLPIMG_5749.jpg',
-      thumbnail: 'src/assets/SLPIMG_5749.jpg',
+      original: 'SLPIMG_5749.jpg',
+      thumbnail: 'SLPIMG_5749.jpg',
     },
     {
-      original: 'src/assets/SLPIMG_5824.jpg',
-      thumbnail: 'src/assets/SLPIMG_5824.jpg',
+      original: 'SLPIMG_5824.jpg',
+      thumbnail: 'SLPIMG_5824.jpg',
     },
     {
-      original: 'src/assets/SLPIMG_5915.jpg',
-      thumbnail: 'src/assets/SLPIMG_5915.jpg',
+      original: 'SLPIMG_5915.jpg',
+      thumbnail: 'SLPIMG_5915.jpg',
     },
     {
-      original: 'src/assets/SLPIMG_5955.jpg',
-      thumbnail: 'src/assets/SLPIMG_5955.jpg',
+      original: 'SLPIMG_5955.jpg',
+      thumbnail: 'SLPIMG_5955.jpg',
     },
     {
-      original: 'src/assets/SLPIMG_5961.jpg',
-      thumbnail: 'src/assets/SLPIMG_5961.jpg',
+      original: 'SLPIMG_5961.jpg',
+      thumbnail: 'SLPIMG_5961.jpg',
     },
   ]
 
+  const createImageUrl = (images) => {
+    return images
+  }
+  
   return (
     <div className='image-gallery-wrapper'>
       <ImageGallery
@@ -177,7 +181,6 @@ function Gallery() {
         showPlayButton={true}
         autoPlay={false}
         slideInterval={5000} 
-          
       />
     </div>
   )
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,42 +3,78 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Landing() {
-  const [actionPhoto, setActionPhoto] = useState();
-  const [specialPhoto, setSpecialPhoto] = useState();
-  const [profilePhoto, setProfilePhoto] = useState();
-  const [aestheticPhoto, setAestheticPhoto] = useState();
-  
+  const [actionPhoto, setActionPhoto] = useState()
+  const [specialPhoto, setSpecialPhoto] = useState()
+  const [profilePhoto, setProfilePhoto] = useState()
+  const [aestheticPhoto, setAestheticPhoto] = useState()
 
-  const actionAndOutDoors = ['/src/assets/CPIMG_5564.jpg', '/src/assets/ISIMG_6077.jpg', '/src/assets/G-IMG_2859.jpg', 'src/assets/G-IMG_2528.jpg', 'src/assets/SLPIMG_5961.jpg']
+  const actionAndOutDoors = [
+    'CPIMG_5564.jpg',
+    'ISIMG_6077.jpg',
+    'G-IMG_2859.jpg',
+    'G-IMG_2528.jpg',
+    'SLPIMG_5961.jpg',
+  ]
 
-  const specialOccassions = ['src/assets/EL236A1709.jpg', 'src/assets/EL236A1769.jpg', 'src/assets/EL236A1788.jpg','src/assets/EL236A1793.jpg']
+  const specialOccassions = [
+    'EL236A1709.jpg',
+    'EL236A1769.jpg',
+    'EL236A1788.jpg',
+    'EL236A1793.jpg',
+  ]
 
-  const profileShots = ['src/assets/G-IMG_4574.jpg', 'src/assets/FAIMG_6846.jpg', 'src/assets/CPIMG_5581.jpg', 'src/assets/EL236A1906.jpg', 'src/assets/FAIMG_6837.jpg']
+  const profileShots = [
+    'G-IMG_4574.jpg',
+    'FAIMG_6846.jpg',
+    'CPIMG_5581.jpg',
+    'EL236A1906.jpg',
+    'FAIMG_6837.jpg',
+  ]
 
-  const aestheticShots = ['src/assets/_MG_0427.jpg', 'src/assets/SFIMG_6600.jpg', 'src/assets/SFIMG_6617.jpg', 'src/assets/G-IMG_2718.jpg', 'src/assets/SLPIMG_5824.jpg']
+  const aestheticShots = [
+    '_MG_0427.jpg',
+    'SFIMG_6600.jpg',
+    'SFIMG_6617.jpg',
+    'G-IMG_2718.jpg',
+    'SLPIMG_5824.jpg',
+  ]
 
-  // useEffect(() => {
-    
-  // })
+  const generateUrl = (imageArray, setPhoto) => {
+    const imageIndex = Math.floor(Math.random() * imageArray.length)
+    const url = import.meta.env.VITE_URLPREFIX + imageArray[imageIndex]
+    setPhoto(url)
+  }
 
   return (
     <div className="landing-page">
       <div className="photo-types">
-        <div className='photo-container' onMouseEnter={() =>  setActionPhoto(actionAndOutDoors[Math.floor(Math.random()* actionAndOutDoors.length)]) }>
+        <div
+          className="photo-container"
+          onMouseEnter={() => generateUrl(actionAndOutDoors, setSpecialPhoto)}
+        >
           <h1>Action & Outdodors</h1>
-          <img className='fill' src={actionPhoto} />
+          <img className="fill" src={actionPhoto} />
         </div>
-        <div className='photo-container' onMouseEnter={() =>  setSpecialPhoto(specialOccassions[Math.floor(Math.random()* specialOccassions.length)]) }>
+        <div
+          className="photo-container"
+          onMouseEnter={() => generateUrl(specialOccassions, setActionPhoto)}
+        >
           <h1>Special Occassions</h1>
-          <img className='fill' src={specialPhoto} />
+          <img className="fill" src={specialPhoto} />
         </div>
-        <div className='photo-container' onMouseEnter={() =>  setProfilePhoto(profileShots[Math.floor(Math.random()* profileShots.length)]) }>
+        <div
+          className="photo-container"
+          onMouseEnter={() => generateUrl(profileShots, setProfilePhoto)}
+        >
           <h1>Profiles</h1>
-          <img className='fill' src={profilePhoto} />
+          <img className="fill" src={profilePhoto} />
         </div>
-        <div className='photo-container' onMouseEnter={() =>  setAestheticPhoto(aestheticShots[Math.floor(Math.random()* aestheticShots.length)]) }>
+        <div
+          className="photo-container"
+          onMouseEnter={() => generateUrl(aestheticShots, setAestheticPhoto)}
+        >
           <h1>Aesthetic</h1>
-          <img className='fill' src={aestheticPhoto} />
+          <img className="fill" src={aestheticPhoto} />
         </div>
       </div>
 
